6c6f3406d5b8d42a9f86f2025792fe624c238082
sudog <sjin0704@gmail.com>
2023-11-20 15:22:51 +0900

    feat(chat): 개발중

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,46 @@
 import json
-
+import g4f as openai
+from constants import content
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatBotConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.messages = [
+            {"role": "system", "content": content},
+        ]
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        user_msg = json.loads(text_data).get("message")
+        bot_msg = self.get_answer(user_msg)
+
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "message": bot_msg,
+                }
+            )
+        )
+
+    async def get_answer(self, user_msg):
+        self.channel_layer
+        response = await openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            provider=openai.Provider.GptGo,
+            messages=self.messages,
+            temperature=2,
+            finish_reason="length",
+            # stream=True,
+        )
+        response = json.loads(response)
+        return response
+
+
+class ChatUserConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         print(self.scope)
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
