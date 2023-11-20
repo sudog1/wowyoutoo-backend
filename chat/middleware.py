15d35689a6202978f5abf829626fc4099299d18d
sudog <sjin0704@gmail.com>
2023-11-20 10:15:37 +0900

    chat: 진행중

--- chat/middleware.py
+++ chat/middleware.py
@@ -5,25 +5,26 @@ from django.contrib.auth.models import AnonymousUser
 
 
 class TokenAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
+
     async def __call__(self, scope, receive, send):
+        scope = dict(scope)
         query_string = scope.get("query_string", b"").decode()
         params = parse_qs(query_string)
 
         access_token = params.get("access")
 
         if access_token:
-            user = self.authenticate(access_token[0])
+            user = self.get_user(access_token[0])
 
             if user:
                 scope["user"] = user
 
         return await self.inner(scope, receive, send)
 
-    def __init__(self, inner):
-        self.inner = inner
-
     @database_sync_to_async
-    def authenticate(self, token):
+    def get_user(self, token):
         try:
             user = TokenModel.objects.get(key=token).user
         except:
