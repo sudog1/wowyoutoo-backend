e6389b19218238dc6a2ac78c7b09b768f18fefb0
sudog <sjin0704@gmail.com>
2023-11-24 06:50:41 +0900

    feat(chat): 토큰 인증 및 채팅기능 완료

--- chat/middleware.py
+++ chat/middleware.py
@@ -1,45 +1,16 @@
 from urllib.parse import parse_qs
-from dj_rest_auth.models import TokenModel
 from channels.db import database_sync_to_async
-from django.contrib.auth.models import AnonymousUser
 from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
-
-class TokenAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
-
-    async def __call__(self, scope, receive, send):
-        scope = dict(scope)
-        query_string = scope.get("query_string", b"").decode()
-        params = parse_qs(query_string)
-
-        access_token = params.get("access")
-
-        if access_token:
-            user = self.get_user(access_token[0])
-
-            if user:
-                scope["user"] = user
-
-        return await self.inner(scope, receive, send)
-
-    @database_sync_to_async
-    def get_user(self, token):
-        try:
-            user = TokenModel.objects.get(key=token).user
-        except:
-            user = AnonymousUser()
-        return user
+from rest_framework_simplejwt.tokens import AccessToken
 
 
 class WebSocketAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         query_string = scope.get("query_string", b"").decode()
         params = parse_qs(query_string)
-        token = params.get("access")
+        access_token = params.get("access")[0]
+        token = AccessToken(access_token)
         user = await self.authenticate_user(token)
         if user is None:
             await self.deny_access(send)
@@ -51,10 +22,7 @@ class WebSocketAuthMiddleware(BaseMiddleware):
     @database_sync_to_async
     def authenticate_user(self, token):
         try:
-            jwt_auth = JWTAuthentication()
-            # validated_token = jwt_auth.get_validated_token(token)
-            print(token)
-            auth_user = jwt_auth.get_user(token)
+            auth_user = JWTAuthentication().get_user(token)
             return auth_user
         except Exception as e:
             return None
--- config/asgi.py
+++ config/asgi.py
@@ -4,7 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chat.middleware import TokenAuthMiddleware, WebSocketAuthMiddleware
+from chat.middleware import WebSocketAuthMiddleware
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
