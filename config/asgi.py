f058270d9b1f782141b7c6f9decd6485d285dccf
sudog <sjin0704@gmail.com>
2023-11-29 19:49:10 +0900

    fix: appregistr 트러블슈팅

--- config/asgi.py
+++ config/asgi.py
@@ -1,18 +1,16 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chat.middleware import WebSocketAuthMiddleware
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import chat.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from chat.middleware import WebSocketAuthMiddleware
+from chat.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter(
     {
