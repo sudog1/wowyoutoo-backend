c71e35c3c162f4fd64173edd954da91ded953207
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-12-06 21:52:47 +0900

    Feat(payments): AUTH_USER_MODEL을 django.conf로 부터 가져옴

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
-from config.settings import AUTH_USER_MODEL
+from django.conf import AUTH_USER_MODEL
 
 
 # Create your models here.       
